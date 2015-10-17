d72ce1b36b55be4a06bf11c994ac727ec16c0016
Kivol <kivoloid@gmail.com>
2015-10-17 22:36:48 +0900

    Added test case

---
+++ test/index.js
@@ -0,0 +1,173 @@
+var assert = require('assert'),
+	express = require('express'),
+	http = require('http'),
+	request = require('request'),
+	swintMiddleware = require('../lib');
+
+// global.swintVar.printLevel = 5;
+
+describe('Loader test', function() {
+	it('Default loads', function() {
+		var loaded = Object.keys(swintMiddleware.middlewares),
+			middlewares = [
+				'body-parser',
+				'cookie-parser',
+				'cors',
+				'favicon',
+				'log-end',
+				'log-start',
+				'method-override',
+				'session',
+				'swint-style-api'
+			];
+
+		middlewares.forEach(function(m) {
+			assert.notEqual(loaded.indexOf(m), -1);
+			assert.equal(swintMiddleware.middlewares[m]({}).length, 3);
+		});
+
+		assert.equal(loaded.length, middlewares.length);
+	});
+});
+
+describe('CORS', function() {
+	var server;
+
+	before(function() {
+		var app = express();
+
+		app.set('config', {
+			http: {
+				validHost: ['localhost']
+			}
+		});
+
+		app.use(swintMiddleware.middlewares.cors());
+
+		app.get('/', function(req, res) {
+			res.send('');
+		});
+
+		server = http.createServer(app);
+		server.listen(8080);
+	});
+
+	it('Check header', function(done) {
+		request.get({
+			url: 'http://localhost:8080/',
+			followRedirect: false
+		}, function(err, resp, body) {
+			assert.equal(resp.headers['access-control-allow-origin'], 'http://localhost');
+			assert.equal(resp.headers['access-control-allow-headers'], 'Origin, X-Requested-With, Content-Type, Accept');
+			assert.equal(resp.headers['access-control-allow-credentials'], 'true');
+			done();
+		});
+	});
+
+	after(function(done) {
+		server.close(function() {
+			done();
+		});
+	});
+});
+
+describe('logger', function() {
+	var server,
+		log;
+
+	before(function() {
+		var app = express();
+
+		app.use(swintMiddleware.middlewares['log-start']());
+
+		app.get('/', function(req, res, next) {
+			res.send('');
+			next();
+		});
+
+		app.use(swintMiddleware.middlewares['log-end']());
+
+		app.use(function(req, res) {
+			log = req.log;
+		});
+
+		server = http.createServer(app);
+		server.listen(8080);
+	});
+
+	it('Check header', function(done) {
+		request.get({
+			url: 'http://localhost:8080/',
+			followRedirect: false
+		}, function(err, resp, body) {
+			var logKeys = Object.keys(log),
+				keys = [
+					'ip',
+					'protocol',
+					'host',
+					'url',
+					'userAgent',
+					'input',
+					'sid',
+					'timestamp',
+					'elapsed',
+					'logTime'
+				];
+
+			logKeys.forEach(function(k) {
+				assert.notEqual(keys.indexOf(k), -1);
+			});
+
+			assert.equal(logKeys.length, keys.length);
+			done();
+		});
+	});
+
+	after(function(done) {
+		server.close(function() {
+			done();
+		});
+	});
+});
+
+describe('swint style api', function() {
+	var server;
+
+	before(function() {
+		var app = express();
+
+		app.use(swintMiddleware.middlewares['swint-style-api']());
+
+		app.get('/', function(req, res, next) {
+			req.out('foo', 'bar');
+			req.success();
+			next();
+		});
+
+		server = http.createServer(app);
+		server.listen(8080);
+	});
+
+	it('Check header', function(done) {
+		request.get({
+			url: 'http://localhost:8080/',
+			followRedirect: false
+		}, function(err, resp, body) {
+			assert.deepEqual(JSON.parse(body), {
+				success: true,
+				session: false,
+				error: null,
+				data: {
+					foo: "bar"
+				}
+			});
+			done();
+		});
+	});
+
+	after(function(done) {
+		server.close(function() {
+			done();
+		});
+	});
+});
